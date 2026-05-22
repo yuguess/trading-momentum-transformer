9923d836317490c8605380e1e27528123e225f70
dcheng <yuguess@gmail.com>
2026-05-22 21:10:22 +0800

    adj fmt

--- data/download_quandl_data.py
+++ data/download_quandl_data.py
@@ -15,16 +15,11 @@ def main(api_key: str):
     for t in ALL_QUANDL_CODES:
         print(t)
         try:
-            data = quandl.get(
-                f"{t}{DEPTH}",
-                start_date="1988-01-01",
-            )
+            data = quandl.get(f"{t}{DEPTH}", start_date="1988-01-01")
         except BaseException as ex:
             print(ex)
         if ("Settle" in data.columns) and (data.index.min() <= dt.datetime(2015, 1, 1)):
-            data[["Settle"]].to_csv(
-                os.path.join("data", "quandl", f"{t.split('/')[-1]}.csv")
-            )
+            data[["Settle"]].to_csv(os.path.join("data", "quandl", f"{t.split('/')[-1]}.csv"))
 
 
 if __name__ == "__main__":
@@ -33,18 +28,11 @@ if __name__ == "__main__":
         """Download the Quandl data"""
 
         parser = argparse.ArgumentParser(description="Download the Quandl data.")
-        parser.add_argument(
-            "api_key",
-            metavar="k",
-            type=str,
-            nargs="?",
-            help="quandl API key",
-        )
+        parser.add_argument("api_key", metavar="k", type=str, nargs="?", help="quandl API key")
 
         args = parser.parse_known_args()[0]
 
-        return (
-            args.api_key,
-        )
-    
+        return (args.api_key,)
+
+    # rKqi69NsAVbsESo9t31C
     main(*get_args())
